abcf9c07f4164f74989afe98e87e1c4d8e0d3286
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-01-01 20:54:32 +0300

    Add ToJson and FromJson in Utf8JsonUtility

--- Packages/UGF.Utf8Json/Runtime/Utf8JsonUtility.cs
+++ Packages/UGF.Utf8Json/Runtime/Utf8JsonUtility.cs
@@ -1,4 +1,6 @@
-﻿using Utf8Json.Resolvers;
+﻿using System;
+using Utf8Json;
+using Utf8Json.Resolvers;
 using Utf8Json.Unity;
 
 namespace UGF.Utf8Json.Runtime
@@ -19,6 +21,28 @@ namespace UGF.Utf8Json.Runtime
             resolver.AddResolver(BuiltinResolver.Instance);
 
             return resolver;
+        }
+
+        public static string ToJson<T>(T target, IUtf8JsonFormatterResolver resolver, bool readable = false)
+        {
+            if (resolver == null) throw new ArgumentNullException(nameof(resolver));
+
+            if (readable)
+            {
+                byte[] bytes = JsonSerializer.Serialize(target, resolver);
+
+                return JsonSerializer.PrettyPrint(bytes);
+            }
+
+            return JsonSerializer.ToJsonString(target, resolver);
+        }
+
+        public static T FromJson<T>(string text, IUtf8JsonFormatterResolver resolver)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentException("Value cannot be null or empty.", nameof(text));
+            if (resolver == null) throw new ArgumentNullException(nameof(resolver));
+
+            return JsonSerializer.Deserialize<T>(text, resolver);
         }
     }
 }
